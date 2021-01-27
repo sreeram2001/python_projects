e211b49322d957becefd4b89d1cc1e5571cc5d14
sreeram2001 <40134790+sreeram2001@users.noreply.github.com>
2021-01-27 20:15:05 +0530

    Add files via upload

--- spell_checker.py
+++ spell_checker.py
@@ -6,17 +6,18 @@ Created on Wed Jan 27 19:29:38 2021
 """
 
 from textblob import TextBlob
+import nltk
+nltk.download('punkt')
 
 
-
-
-
-ip = input("Enter Sentence")
-wordlist = ip.strip()
+ip = input("Enter Sentence \n")
+print("Sentence Entered : ",ip)
+wordlist = ip.split()
 corrected = []
-print(wordlist)
+
 for word in wordlist:
     corrected.append(TextBlob(word))
-for e in corrected:
-    print(e.correct(), end=" ")
 
+print("Corrected Sentence Is: ")
+for e in corrected:
+    print(str(e.correct()), end=" ")   
